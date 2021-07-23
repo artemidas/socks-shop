4df0aa9e8ede625244aa7ea3f43db5fd30893afe
artemidas <artem.pasichnyk@hotmail.com>
2021-07-23 15:01:38 +0200

    updates

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'I love this product',
+      image: './assets/images/socks_green.jpg',
+      url: 'https://devrookie.netlify.app/',
+      inventory: 100
+    }
+  }
+}).mount('#app')
